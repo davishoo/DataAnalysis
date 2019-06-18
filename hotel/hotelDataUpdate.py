a58991d33fe9a55cf974f0fcc03274ffddc14fca
Davis <50162710+davishoo@users.noreply.github.com>
2019-06-18 12:26:38 +0800

    Update hotelDataUpdate.py

--- hotel/hotelDataUpdate.py
+++ hotel/hotelDataUpdate.py
@@ -1,19 +1,36 @@
 import pandas as pd
-hotel = pd.ExcelFile('hotelData.xlsx')
-hotelDF = pd.read_excel(hotel, 'Sheet1')
+import os
+
+def sourcePath():
+    # import os
+    print('Pls specify the path of input file ...')
+    print('Example: D:\\file\\python\\AutomatePDF')
+    path = input()   # call for input
+    sourceFolder = os.path.abspath(path) # make sure the path is absolute one
+    return sourceFolder
+
+sourceFile = 'hotelData.xlsx' 
+absPath =  os.path.join(sourcePath(),sourceFile)
+
+    
+inputObj = pd.ExcelFile(absPath)    # specify input data source
+hotelDF = pd.read_excel(inputObj, 'Sheet1')
 
 level_1 = (('基本信息',) * 5) + (('酒店销售',) * 6)
 level_2 = ('酒店名称', '酒店位置', '详细地址', '前台电话', '客房总数', '姓名', 
                     '手机', '座机', '微信','QQ','Email')
 
-hotelDF.columns = [list(level_1), list(level_2)]
+hotelDF.columns = [list(level_1), list(level_2)]  # restructure column names
 
+hotelDF.to_csv('hotelDB.csv',encoding='utf_8_sig',index=False)  # save to csv
+
+output = pd.read_csv('hotelDB.csv', header=[0,1])
 writer = pd.ExcelWriter('酒店完整信息表.xlsx')
-hotelDF.to_excel(writer, 'Sheet1')
+output.to_excel(writer, 'Sheet1')
 writer.save()
 
 writer = pd.ExcelWriter('酒店基本信息表.xlsx')
-hotelDF.基本信息.to_excel(writer, 'Sheet1', index = False)
+output.基本信息.to_excel(writer, 'Sheet1', index = False)
 writer.save()
 
 print('done')
